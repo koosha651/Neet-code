d107e2ef5774632d4b729fbed9081a4ef167315f
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-05-25 18:56:09 -0400

    two sum

--- NeetCode.py
+++ NeetCode.py
@@ -2,20 +2,20 @@
 #Solving Problem from NeetCode website
 
 # 1. Contains Duplicate (https://leetcode.com/problems/contains-duplicate/submissions/)
-if len(nums) != len(set(nums)): #set() prevents redundancy  
+if len(nums) != len(set(nums)): #set() prevents redundancy
     return True
 
 # 2. Valid Anagram
 #we should create 2 hashmaps and the total time complexity will be O(S + T) which is the iteration in these tow hashmaps. **Downside: potential high memory usage**
 
 
-countS , countT = {},{}     #defining two seperate hash table for each list and check whether they are have same length 
-if len(s)!= len(T): 
+countS , countT = {},{}     #defining two seperate hash table for each list and check whether they are have same length
+if len(s)!= len(T):
     return False
     for i in range(len(s)):
-        countS[s[i]] = 1 + countS.get(s[i],0)   #each time we see a character we want to increment that by 1 and 0 if that charecter has not appereard 
+        countS[s[i]] = 1 + countS.get(s[i],0)   #each time we see a character we want to increment that by 1 and 0 if that charecter has not appereard
         countT[t[i]] = 1 + countT.get(t[i],0)
-        
+
     for c in countS:
         if countS[c] != countT.get(c,0):
             return False
@@ -23,5 +23,8 @@ if len(s)!= len(T):
 #  /////////////////////////////
 return Counter(s) == Counter(t)
 # /////////////////////////////
-return sorted(s)==sorted(t)  # use seaching algorithm time complexity O(n^2)                
-        
+return sorted(s)==sorted(t)  # use seaching algorithm time complexity O(n^2)
+
+
+
+# we're looking for indices, so sorting is not necessary. Mostly using dictionary (hastable) helps.
