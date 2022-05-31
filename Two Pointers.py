1a29eee71bc97905c9d26aebc4c66f29fa67b9bd
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-05-30 23:04:30 -0400

    notebook edit may 30

--- Two Pointers.py
+++ Two Pointers.py
@@ -1,3 +1,16 @@
 
 # NeetCode Challenge
 # Solving Problems from NeetCode website, Tow Pointers
+
+# 1. Valid Palindrome
+# Two ways to solve the problem. First method use extra memory. Second methods 
+
+s = "A man, a plan, a canal: Panama"
+
+def isPalindrome(s: str) -> bool:
+    strs= ''
+    for c in s:
+        if c.isalnum():
+            strs += c.lower()
+    return strs == strs[::-1]
+        
