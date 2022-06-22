207829e57450cce5fdd0769c3531d5298ad81f9a
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-06-22 17:24:56 -0400

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -2,5 +2,14 @@
 # NeetCode Challenge
 # Solving Problems from NeetCode website, Binary Search
 
+Binary search algorithm basic ideas:
+
++ The array has to be already sorted
++  divide and conquer technique, which is repeatedly breaks down the array into two subarrays
+
+
+
+#=============================================================================================================================================================================
+  
 
 1. Binary search
