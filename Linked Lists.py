fb79d84bec7a4a67b27706fa2e1ca1c98f18f08c
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-06-27 18:35:11 -0400

    Update Linked Lists.py

--- Linked Lists.py
+++ Linked Lists.py
@@ -2,8 +2,35 @@
 # Solving Problems from NeetCode website, Linked Lists
 
 https://www.youtube.com/watch?v=njTh_OwMljA&t=141s
-  
-  
+
+
 #=============================================================================================================================================================================
- 
+
 1. Reverse Linked List
+
+# Total time complexity is : O(n)
+# Total memory complexity :  O(1)
+method_#1: Iterative                                                                                method_#2: Recursive
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev, cur = None, head
+
+        while cur:          # we want to keep going untill we reach the end of the List
+
+            # locate next hoppoing node
+            next_hop = cur.next
+
+            # reverse direction
+            cur.next = prev
+
+            # reverse pointers
+            prev = cur
+            cur = next_hop
+
+        # new head of reverse linked list
+        return prev
+
+
+#=============================================================================================================================================================================
