65090fc9d9181ab6c2205076412e7f609c39dbc5
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-06-29 17:58:36 -0400

    Update Trees.py

--- Trees.py
+++ Trees.py
@@ -5,3 +5,7 @@
 https://www.youtube.com/watch?v=7tCNu4CnjVc
 
 #=============================================================================================================================================================================
+
+1.  Invert Binary Tree
+
+# the conceot is to visit every single node in the tree and then if each of these nodes has children, we must swop their children. So we can sove this problem with recursion as DFS
