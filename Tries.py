5e0f79a4800d47ff78ac133243d885d8552e56e5
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-13 14:17:37 -0400

    Update Tries.py

--- Tries.py
+++ Tries.py
@@ -2,5 +2,6 @@
 # NeetCode Challenge
 # Solving Problems from NeetCode website, Tries
 
-
+A Trie (prefix tree) is a special data structure used to store strings that can be visualized like a graph. There are various applications of this data structure, such 
+as autocomplete and spellchecker
 #=============================================================================================================================================================================
