bfac40fb59c709b0b74e7d0b091d307a06d3a8fd
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-14 18:21:22 -0400

    Update Tries.py

--- Tries.py
+++ Tries.py
@@ -12,24 +12,44 @@ Tries is useing for word validation
 
 # Create seperate class named TriesNode: include children, endOfWord
 
-class TrieNode:
+
+class TrieNode:                         # build constractor for initialize with a base root
     def __init__(self):
-        self.children = {}
-        self.endOfNode = False
+        self.children = {}              # each node comes with it's own children. it can be 26 children
+        self.endOfNode = False          # mark the last single node as end of a word
 
 class Trie:
 
     def __init__(self):
-        
-        self.root
+
+        self.root = TrieNode()          # initialize the class with a root
 
     def insert(self, word: str) -> None:
-        
+        cur = self.root                 # initialize our current node to start from the root
+
+        for c in word:                         # go trought every single charecter of the word and check 2 thing: does the charecter already exist or not
+            if c not in cur.children:                    # if the charecter doesn't exist in our current hashmap,
+                cur.children[c] = TrieNode()             # will create a Trie node for that. use charecter as key value and then create empty trie node
+            cur = cur.children[c]                  # but if the charecter does already exist, set the curent to that node
+        cur.endOfNode = True            # after iterating trough the word, set the last character as end of the word
 
     def search(self, word: str) -> bool:
-        
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfNode
+
 
     def startsWith(self, prefix: str) -> bool:
-        
+        cur = self.root
 
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
 
+#=============================================================================================================================================================================
