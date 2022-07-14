fab36798c9f0504325739eb9b227ef22ddd3c3f2
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-14 16:26:07 -0400

    Update Tries.py

--- Tries.py
+++ Tries.py
@@ -2,6 +2,12 @@
 # NeetCode Challenge
 # Solving Problems from NeetCode website, Tries
 
-A Trie (prefix tree) is a special data structure used to store strings that can be visualized like a graph. There are various applications of this data structure, such 
+A Trie (prefix tree) is a special data structure used to store strings that can be visualized like a graph. There are various applications of this data structure, such
 as autocomplete and spellchecker
+Tries is useing for word validation
 #=============================================================================================================================================================================
+
+# The child of each node must be unique so at most for each node we have 26 unique child wich represent the lenght of english alphabet. Also, we dealing with 4 sub-function
+# such as insert(), search(), startWith(). one of the advantage of using Tries is the startWith() function, which in compare to array reduce memory usage and time complexity
+
+# Create seperate class named TriesNode: include children, endOfWord
