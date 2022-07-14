72b1d9fd226296f0d923ea0099cc9088be241713
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-14 17:39:51 -0400

    Update Tries.py

--- Tries.py
+++ Tries.py
@@ -11,3 +11,25 @@ Tries is useing for word validation
 # such as insert(), search(), startWith(). one of the advantage of using Tries is the startWith() function, which in compare to array reduce memory usage and time complexity
 
 # Create seperate class named TriesNode: include children, endOfWord
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfNode = False
+
+class Trie:
+
+    def __init__(self):
+        
+        self.root
+
+    def insert(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
+
+
