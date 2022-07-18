7e668d62d19a4777547e6abcb2bf12f30cdbf2cb
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-18 15:20:57 -0400

    Create Heap & Priority Queue.py

---
+++ Heap & Priority Queue.py
@@ -0,0 +1,9 @@
+
+# NeetCode Challenge
+# Solving Problems from NeetCode website, Heap & Priority Queue
+
+# by adding ' import heapq ' you can use the heap 
+arr[(k - 1)/2] will return the parent node   
+arr[(2*k) + 1] will return left child
+arr[(2*k) + 2] will return right child
+#=============================================================================================================================================================================
