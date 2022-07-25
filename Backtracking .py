736e8f6f21ae695daae03d0350f7b6e9d314505c
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-24 20:32:00 -0400

    Update Backtracking .py

--- Backtracking .py
+++ Backtracking .py
@@ -2,7 +2,7 @@
 # NeetCode Challenge
 # Solving Problems from NeetCode website, Backtracking 
 
-https://www.youtube.com/watch?v=DKCbsiDBN6c
+# https://www.youtube.com/watch?v=DKCbsiDBN6c
 
 BFS ----- > traversal -------> in a level by level manner                   DFS ----- > traversal -------> in a depth from the root node to bottom (Backtracking)
 
@@ -14,6 +14,8 @@ BFS ----- > traversal -------> in a level by level manner                   DFS 
 # for solving this question there are two ways:
 # for each input we could include it in the subset or not, so for each input we have two choice and for array with 'n' input we have 2^n choice for our subset. 
 
+#  first methods:
+
 nums = [1 ,2, 3]
 def subset(nums):
     output = [[]]
@@ -21,10 +23,19 @@ def subset(nums):
         output += [lst + [i] for lst in output]
     return output
     
+ #++++++++++++++++++++++++++++++++
+
+#  second method:
+
+def subsets(nums):
     
-
-
-
+    res = []
+    subset = []
+    def dfs(i):
+        if i >= len(nums):
+            res.append
+    
+    
     
 
 #=============================================================================================================================================================================
