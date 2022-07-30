1df010d31da0ebadad157c7ce58498114d4b2760
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-30 18:53:14 -0400

    Update Backtracking .py

--- Backtracking .py
+++ Backtracking .py
@@ -130,3 +130,32 @@ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 
 #=============================================================================================================================================================================
+
+4. subset II
+
+# in order to prevent
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            # All subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            # All subsets that don't include nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+
+
+#=============================================================================================================================================================================
