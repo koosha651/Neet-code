e73eb28a12d907e5ef47decd661529ca5466764e
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-07-31 18:09:18 -0400

    Update Graph.py

---
+++ Graph.py
@@ -0,0 +1,12 @@
+# NeetCode Challenge
+# Solving Problems from NeetCode website, Graph
+
+We use BFS & DFS for solving Graph related problems.
+
+BFS uses Queue and DFS uses stack.
+primary difference between Stack and Queue:      Stack follows LIFO  (Last In First Out)    &     Queue follows FIFO (First In First Out)
+
+https://stackoverflow.com/questions/10974922/what-is-the-basic-difference-between-stack-and-queue   ---->(picture)
+
+
+#=============================================================================================================================================================================
