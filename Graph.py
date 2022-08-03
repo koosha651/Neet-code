de5c2b532aa1458517cd33a56f166563e6851907
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-08-03 14:01:12 -0400

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -3,10 +3,16 @@
 
 We use BFS & DFS for solving Graph related problems.
 
-BFS uses Queue and DFS uses stack.
-primary difference between Stack and Queue:      Stack follows LIFO  (Last In First Out)    &     Queue follows FIFO (First In First Out)
+BFS uses Queue                                                                            DFS uses stack
+BFS is iterative                                                                          DFS is recursive
+Queue follows FIFO (First In First Out)                                                   Stack follows LIFO  (Last In First Out)         
+
 
 https://stackoverflow.com/questions/10974922/what-is-the-basic-difference-between-stack-and-queue   ---->(picture)
 
 
 #=============================================================================================================================================================================
+
+1. Number of Islands
+
+ROWS, COLS = len(grid)
