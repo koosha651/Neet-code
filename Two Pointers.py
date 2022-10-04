da84670f511b8e16f87255da935b719f1061602a
koosha651 <84024555+koosha651@users.noreply.github.com>
2022-10-04 13:33:15 -0400

    Update Two Pointers.py

--- Two Pointers.py
+++ Two Pointers.py
@@ -123,3 +123,23 @@ maxArea(height)
 # TIME COMPLEXITY = O(n^2)
 
 &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+height = [1,8,6,2,5,4,8,3,7]
+
+def maxArea(height):
+    
+    res = 0
+    l,r = 0 , len(height)-1
+    
+    while l< r:
+        area = (r-l) * min(height[l], height[r])
+        res = max(res, area)
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return res
+
+maxArea(height)
+
+# linearn time solution: O(n)
