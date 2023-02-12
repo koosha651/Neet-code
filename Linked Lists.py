5f8c4a678e57a986a6dd26c9fc120df9c1fd230e
koosha651 <84024555+koosha651@users.noreply.github.com>
2023-02-11 19:37:21 -0500

    Update Linked Lists.py

--- Linked Lists.py
+++ Linked Lists.py
@@ -72,3 +72,29 @@ class Solution:
 
 
 # we solve this question in 3 steps         1. Finding the middle       2. reversing the pointers in the second half of list        3. merging two half
+
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        # find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
